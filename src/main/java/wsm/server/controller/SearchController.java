ae91c147411b6e729f47f5ab3873c7c6ba999cd4
zhengjilai <zhengjilai5816@163.com>
2020-06-02 16:15:54 +0800

    Add dependencies for spring boot

---
+++ src/main/java/wsm/server/controller/SearchController.java
@@ -0,0 +1,16 @@
+package wsm.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SearchController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
