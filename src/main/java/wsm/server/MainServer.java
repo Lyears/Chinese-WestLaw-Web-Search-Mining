4d94afbbf5f42b1b3d2547607826b9810cac04d6
zmfan <805106298@qq.com>
2020-06-03 21:28:58 +0800

    Add index.html

--- src/main/java/wsm/server/MainServer.java
+++ src/main/java/wsm/server/MainServer.java
@@ -1,12 +1,14 @@
 package wsm.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class MainServer {
 
     public static void main(String[] args) {
         SpringApplication.run(MainServer.class, args);
+
     }
 
 }
--- src/main/java/wsm/server/controller/SearchController.java
+++ src/main/java/wsm/server/controller/SearchController.java
@@ -3,14 +3,15 @@ package wsm.server.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SearchController {
 
-    @ResponseBody
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World!";
+    @RequestMapping("/")
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("index");
+        return mav;
     }
 
 }
